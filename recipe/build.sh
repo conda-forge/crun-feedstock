7da6331566c09b9213bd30e900135587154f0b71
UENO, M. <eunosium-1128@live.jp>
2026-01-22 02:21:15 +0900

    Refactor build script for platform-specific configuration

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,22 @@
 #! /usr/bin/env bash
 
 autoreconf -fis
-./configure \
-    --prefix="${PREFIX}" \
-    --disable-systemd \
-    --disable-static \
-    --enable-shared \
-    --disable-dynload-libcrun
+
+if [[ ${build_platform} != ${target_platform} ]]; then
+    ./configure \
+        --prefix="${PREFIX}" \
+        --disable-systemd \
+        --disable-shared \
+        --enable-static \
+        --disable-dynload-libcrun
+else
+    ./configure \
+        --prefix="${PREFIX}" \
+        --disable-systemd \
+        --disable-static \
+        --enable-shared \
+        --disable-dynload-libcrun
+fi
+
 make -j"${CPU_COUNT}"
 make install
